03c9ae8e9dec08ab9ea1aacb5276377dde84c847
F99 <farajiomar99@gmail.com>
2026-01-29 11:33:58 +0100

    feat: define application routes for main and car details components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CarsList } from './shared/components/cars-list/cars-list';
+import { AddCar } from './shared/components/add-car/add-car';
+import { Main } from './shared/layout/main/main';
+import { CarDetails } from './shared/components/car-details/car-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Main
+    },
+    {
+        path: 'cars',
+        component: Main
+    },
+    {
+        path: 'car/:id',
+        component: CarDetails
+    }
+    
+];
