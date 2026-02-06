8f0cad06ec7fb17c0eb6abec26e3141ff0d3017b
F99 <farajiomar99@gmail.com>
2026-02-06 12:08:21 +0100

    feat: create home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { CarsList } from './shared/components/cars-list/cars-list';
 import { AddCar } from './shared/components/add-car/add-car';
 import { Main } from './shared/layout/main/main';
 import { CarDetails } from './shared/components/car-details/car-details';
+import { Home } from './shared/layout/home/home';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Main
+        component: Home
     },
     {
         path: 'cars',
-        component: Main
+        loadComponent() {
+            return import('./shared/layout/main/main').then(m => m.Main);
+        },
     },
     {
         path: 'car/:id',
